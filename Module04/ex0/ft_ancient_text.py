40af23e3e8a6ba360f5c172930635877c2a5e7c7
Artem Pysmak <arpysmak@c3r8s4.42urduliz.com>
2026-04-29 16:42:08 +0200

    Module04 | Quick fix; Completed ex1

--- Module04/ex0/ft_ancient_text.py
+++ Module04/ex0/ft_ancient_text.py
@@ -30,7 +30,7 @@ def main():
 
     except ArchiveError as e:
         print(f"Error opening file '{arg}': {e.message}")
-
+        return
     finally:
         if file is not None:
             file.close()
--- Module04/ex1/ft_archive_creation.py
+++ Module04/ex1/ft_archive_creation.py
@@ -16,20 +16,25 @@ def main():
 
     print("=== Cyber Archives Recovery & Preservation ===")
     print(f"Accessing file '{arg}'")
+
     file = None
+    lines = []
+
     try:
         try:
             file = open(arg, 'r', encoding='utf-8')
+            lines = file.readlines()
         except (FileNotFoundError, PermissionError) as e:
             raise ArchiveError(str(e))
 
-        print("---")
-        content = file.read()
-        print(content.strip())
-        print("---")
+        print("---\n")
+        content_original = "".join(lines)
+        print(content_original.strip())
+        print("\n---")
 
     except ArchiveError as e:
         print(f"Error opening file '{arg}': {e.message}")
+        return
 
     finally:
         if file is not None:
@@ -37,6 +42,37 @@ def main():
             print(f"File '{arg}' closed.")
 
     print("\nTransform data:")
+    print("---\n")
+
+    data_transform = ""
+
+    for line in lines:
+        mod_line = line.rstrip('\n\r') + '#\n'
+
+        data_transform += mod_line
+        print(mod_line, end='')
+    data_transform.rstrip('\n\n')
+
+    print("\n---")
+
+    new_file = input("Enter new file name (or empty): ")
+    new_archive = None
+
+    if new_file == '':
+        print("Not saving data.")
+        return
+
+    try:
+        print(f"Saving data to '{new_file}'")
+        new_archive = open(new_file, 'w', encoding='utf-8')
+        new_archive.write(data_transform)
+        print(f"Data saved in file '{new_file}'\n")
+    except Exception as e:
+        print(f"Save error: {e}")
+        return
+    finally:
+        if new_archive is not None:
+            new_archive.close()
 
 
 if __name__ == "__main__":
